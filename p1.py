cba2b923416c167f5abfe8fee4c227119fe7ac76
vignesh07 <vignesh.natarajan92@gmail.com>
2014-10-27 21:26:37 -0700

    p2

--- p1.py
+++ p1.py
@@ -1,9 +1,22 @@
 import networkx as nx 
 import matplotlib.pyplot as plt
+import operator
 
 G=nx.read_edgelist("anoncsv.csv",'rb',delimiter=',')
 
-"""count=0
+
+degree_sequence=sorted(nx.degree(G).values(),reverse=True) # degree sequence
+#print "Degree sequence", degree_sequence
+dmax=max(degree_sequence)
+
+plt.loglog(degree_sequence,'b-',marker='o')
+plt.title("Degree rank plot")
+plt.ylabel("degree")
+plt.xlabel("rank")
+
+plt.savefig("degree_histogram.png")
+
+count=0
 tri=nx.triangles(G)
 x=tri.values()
 
@@ -12,34 +25,8 @@ for xx in x:
 	count=count+int(xx)
 
 print count
-"""
-"""i=1
-count=0
-while(i<1001):
-	count = count + tri[i]
-	print count
-	i=i+1
 
-print "count", count"""
-"""degree_sequence=sorted(nx.degree(G).values(),reverse=True) 
-dmax=max(degree_sequence)
-plt.loglog(degree_sequence,'b-',marker='o')
-print dmax
-plt.savefig('degree_dist.png')
-plt.show()"""
-""""g=nx.DiGraph(G)
-count=0
-for n in nx.simple_cycles(g):
-	print list(n)
-	if len(list(n))==3:
-		print list(n)
-		count=count+1
-		print count
+T=nx.minimum_spanning_tree(G,weight='weight')
+print T.edges(data=True)
 
-print count"""
-
-
-d=nx.diameter(G)
-
-
-print "\n diameter", d
+##print ccc-sss
